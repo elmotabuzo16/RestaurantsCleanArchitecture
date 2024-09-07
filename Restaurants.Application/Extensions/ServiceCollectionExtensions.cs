126c8a59e718a560aa2837708f18cacf5b602fc9
Elmo Tabuzo <elmotabuzojr.ust@gmail.com>
2024-09-07 12:54:58 +0800

    Fluent Validation

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 using Restaurants.Domain.Repositories;
 using System;
@@ -13,8 +15,11 @@ namespace Restaurants.Application.Extensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
+            var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
             services.AddScoped<IRestaurantsService, RestaurantsService>();
-            services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+            services.AddAutoMapper(applicationAssembly);
+            services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
         }
     }
 }
